5f67a490c73345045766b5714d858ad22a1193a5
Prasad <prasad.sawantdesai@gmail.com>
2026-07-09 15:57:08 +0200

    Update idstools/view/common.py
    Co-authored-by: Simon Pinches <SimonPinches@users.noreply.github.com>

--- idstools/view/common.py
+++ idstools/view/common.py
@@ -261,7 +261,7 @@ class PlotCanvas:
                 from IPython.display import display
 
                 display(self.fig)
-                if backend != "module://matplotlib_ipympl.backend_nbagg":
+                if backend != "module://ipympl.backend_nbagg":
                     plt.close("all")
             except ImportError:
                 pass
